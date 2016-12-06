5f52439bf292f4e46db4c3238c9c95d634cfe915
Raphael  Kinoti <kinoti.raphs@gmail.com>
2016-12-06 12:17:42 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,4 +5,4 @@ sudo usermod -aG docker ubuntu
 echo "docker_OPTS="--dns 8.8.8.8"" >> /etc/default/docker
 
 #Install webservice congtainer
-docker build https://github.com/RaphaelKinoti/basic.git -t webContainer
+docker build -t webContainer https://github.com/RaphaelKinoti/basic.git
