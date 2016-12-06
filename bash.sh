5e9406e1641f022d7a45e70f400c18af5977dbb4
Raphael  Kinoti <kinoti.raphs@gmail.com>
2016-12-06 13:06:06 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,5 +5,5 @@ sudo usermod -aG docker ubuntu
 echo "docker_OPTS="--dns 8.8.8.8"" >> /etc/default/docker
 
 #Install webservice congtainer
-docker build -t webimage https://raphaelkinoti@github.com/raphaelkinoti/basic.git
+docker build -p 8080:8080 -t webimage https://raphaelkinoti@github.com/raphaelkinoti/basic.git
 
