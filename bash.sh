0958bff144552dc0b2d095df18a489dff4e3dc98
Raphael  Kinoti <kinoti.raphs@gmail.com>
2016-12-06 12:00:03 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,8 @@
+#Install docker
 curl -fsSL https://get.docker.com/ | sh
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 echo "docker_OPTS="--dns 8.8.8.8"" >> /etc/default/docker
+
+#Install webservice congtainer
+docker build https://github.com/RaphaelKinoti/basic.git
