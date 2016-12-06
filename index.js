0e6892de6f7899ab65f6a93a71ff1663d97fd2bc
Raphael  Kinoti <kinoti.raphs@gmail.com>
2016-12-06 13:01:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var express = require('express')
-var app = express()
+//Lets require/import the HTTP module
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+//Lets define a port we want to listen to
+const PORT=8080; 
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
